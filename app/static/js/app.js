95a4ccdeb0d9d0c7dc18efa800a03a3c9789a2fc
Jeff Roche <jkeauctions@gmail.com>
2015-03-08 18:46:40 -0400

    Some modifications testin gin global namespace

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -2,85 +2,84 @@
 
 var app = app || {};
 
-$(function() {
+// our global pubSub object
+var pubSub = _.extend({},Backbone.Events);
 
-    // our global pubSub object
-    var pubSub = _.extend({},Backbone.Events);
-
-    // Tab Model
-    var Tab = Backbone.Model.extend({
-        
-        defaults: {
-            id: '',
-            active: false
-        }
-    });
-
-    // Tab Collection
-    var TabList = Backbone.Collection.extend({
-        
-        // reference to this collections model.
-        model: Tab
-
-    });
-
-    // create our global collection of tabs
-    var Tabs = new TabList;
-        
-    // Tab View item
-    var TabView = Backbone.View.extend({
-        id: "",
-
-        active: false,
-
-        events: {
-            "click": "publish",
-        },
-
-        initialize: function() {
-
-            // listen for changes on the following Tab Model events.
-            this.listenTo(pubSub, "tab:selected", this.clicked, this);
-        },
-
-        clicked: function(tab) {
-            if (tab.selected === this.id){
-                this.$el.addClass('active');
-            } else {
-                this.$el.removeClass('active');
-            }
-        },
-            
-        publish: function() {
-            pubSub.trigger("tab:selected", {selected: this.id});
-        }
+// Tab Model
+var Tab = Backbone.Model.extend({
     
-    });
+    defaults: {
+        id: '',
+        active: false
+    }
+});
 
-    // The application    
-    var AppView = Backbone.View.extend({
+// Tab Collection
+var TabList = Backbone.Collection.extend({
+    
+    // reference to this collections model.
+    model: Tab
 
-        // Bind to the existing skeleton of the App already present in the HTML.
-        el: $("#marstime"),
+});
 
-        // Initialize all tab views and models in the collections.
-        initialize: function() {
+// create our global collection of tabs
+var Tabs = new TabList;
+    
+// Tab View item
+var TabView = Backbone.View.extend({
 
-            // listen for changes on the following Tabs Collection events.
-            this.listenTo(Tabs, 'add', this.addTabView);
+    active: false,
 
-            // initialize all tabs
-            Tabs.add(tabsData);
+    events: {
+        "click": "publish",
+    },
 
-        },
+    initialize: function() {
 
-        // Add a single tab view.
-        addTabView: function(tab) {
-            var view = new TabView({model: tab});
+        // listen for changes on the following Tab Model events.
+        this.listenTo(pubSub, "tab:selected", this.clicked, this);
+    },
+
+    clicked: function(tab) {
+        if (tab.selected === this.id){
+            this.$el.addClass('active');
+        } else {
+            this.$el.removeClass('active');
         }
-    });
+    },
+        
+    publish: function() {
+        console.debug('Tabview.publish:' + this.id);
+        pubSub.trigger("tab:selected", {selected: this.id});
+    }
 
+});
+
+// The application    
+var AppView = Backbone.View.extend({
+
+    // Bind to the existing skeleton of the App already present in the HTML.
+    el: $("#marstime"),
+
+    // Initialize all tab views and models in the collections.
+    initialize: function() {
+
+        // listen for changes on the following Tabs Collection events.
+        this.listenTo(Tabs, 'add', this.addTabView, this);
+
+        // initialize all tabs
+        Tabs.add(tabsData);
+
+    },
+
+    // Add a single tab view.
+    addTabView: function(tab) {
+        var view = new TabView({model: tab});
+        console.log(view);
+    }
+});
+
+$(function() {
     // create the App.
     var App = new AppView;
-
 });
